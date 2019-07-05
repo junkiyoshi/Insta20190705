0af04bad2a5cd3bae3cd37efa7271e9312b3f4a2
中内 純 <nakauchi.k@g-hits.co.jp>
2019-07-05 13:11:16 +0900

    First

---
+++ ofApp.cpp
@@ -0,0 +1,73 @@
+﻿#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofEnableDepthTest();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+	ofRotateY(ofGetFrameNum() * 0.5);
+
+	for (int x = -150; x <= 150; x += 150) {
+
+		for (int y = -150; y <= 150; y += 150) {
+
+			this->drawNoiseBox(glm::vec3(x, y, 0), 80);
+		}
+	}
+
+	this->cam.end();
+}
+
+//--------------------------------------------------------------
+void ofApp::drawNoiseBox(glm::vec3 location, int length) {
+
+	ofPushMatrix();
+	ofTranslate(location);
+
+	int size = 20;
+	for (int x = -length * 0.5; x <= length * 0.5; x += size) {
+
+		for (int y = -length * 0.5; y <= length * 0.5; y += size) {
+
+			for (int z = -length * 0.5; z <= length * 0.5; z += size) {
+
+				auto box_location = glm::vec3(x, y, z);
+				auto noise_value = ofNoise(location.x + x * 0.005, location.y + y * 0.005, location.z + z * 0.005, ofGetFrameNum() * 0.005);
+
+				ofNoFill();
+				ofSetColor(39);
+				ofDrawBox(box_location, size * 0.9);
+
+				if (noise_value > 0.5) {
+
+					ofFill();
+					ofSetColor(239);
+					ofDrawBox(box_location, size * 0.88);
+				}
+			}
+		}
+	}
+
+	ofPopMatrix();
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
